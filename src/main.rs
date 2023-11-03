b8853a3dea65e720fc75a5cfbca10e134dbf4c19
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-11-03 11:49:34 +0900

    [Add]: accept simple arithmetic expressions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 #[derive(Debug)]
 enum CompilerError {
     InvalidNumberOfArguments,
-    InvalidNumberFormat,
+    InvalidExpression,
 }
 
 fn main() -> Result<(), CompilerError> {
@@ -13,18 +13,50 @@ fn main() -> Result<(), CompilerError> {
         return Err(CompilerError::InvalidNumberOfArguments);
     }
 
-    let num = match args[1].parse::<i32>() {
-        Ok(num) => num,
-        Err(_) => {
-            eprintln!("The number format is invalid");
-            return Err(CompilerError::InvalidNumberFormat);
-        }
-    };
-
     println!(".intel_syntax noprefix");
     println!(".global main\n");
     println!("main:");
+
+    let expr_bytes = args[1].as_bytes();
+    let mut i = 0;
+    let mut num = 0;
+    while i < expr_bytes.len() && expr_bytes[i].is_ascii_digit() {
+        num = num * 10 + (expr_bytes[i] - '0' as u8) as i32;
+        i += 1;
+    }
     println!("  mov rax, {:?}", num);
+    num = 0;
+    let mut prev_operator_is_plus = true;
+    let mut first_operator = true;
+    while i < expr_bytes.len() {
+        let c = expr_bytes[i];
+        if c.is_ascii_digit() {
+            num = num * 10 + (c - '0' as u8) as i32;
+            i += 1;
+            continue;
+        } else if c == ('+' as u8) || c == ('-' as u8) {
+            if first_operator {
+                first_operator = false;
+            } else if prev_operator_is_plus {
+                println!("  add rax, {:?}", num);
+            } else {
+                println!("  sub rax, {:?}", num);
+            }
+            num = 0;
+            prev_operator_is_plus = c == ('+' as u8);
+            i += 1;
+            continue;
+        } else {
+            eprintln!("The number format is invalid");
+            return Err(CompilerError::InvalidExpression);
+        }
+    }
+    if !first_operator && prev_operator_is_plus {
+        println!("  add rax, {:?}", num);
+    } else {
+        println!("  sub rax, {:?}", num);
+    }
+
     println!("  ret");
     Ok(())
 }
