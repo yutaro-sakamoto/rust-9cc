58435d5d44a0beefc1a8b738c184163c5d00fbe9
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-11-18 18:50:56 +0900

    [Add]: add assembly functions for shortcuts

--- src/assembly.rs
+++ src/assembly.rs
@@ -51,11 +51,79 @@ impl fmt::Debug for Instruction {
     }
 }
 
+pub fn push(operand: Operand) -> Instruction {
+    Instruction::Push(operand)
+}
+
+pub fn pop(operand: Operand) -> Instruction {
+    Instruction::Pop(operand)
+}
+
+pub fn ret() -> Instruction {
+    Instruction::Ret
+}
+
+pub fn add(operand1: Operand, operand2: Operand) -> Instruction {
+    Instruction::Add(operand1, operand2)
+}
+
+pub fn sub(operand1: Operand, operand2: Operand) -> Instruction {
+    Instruction::Sub(operand1, operand2)
+}
+
+pub fn mul(operand: Operand) -> Instruction {
+    Instruction::Mul(operand)
+}
+
+pub fn idiv(operand: Operand) -> Instruction {
+    Instruction::Idiv(operand)
+}
+
+pub fn cqo() -> Instruction {
+    Instruction::Cqo
+}
+
+pub fn neg(operand: Operand) -> Instruction {
+    Instruction::Neg(operand)
+}
+
+pub fn movzb(operand1: Operand, operand2: Operand) -> Instruction {
+    Instruction::Movzb(operand1, operand2)
+}
+
+pub fn mov(operand1: Operand, operand2: Operand) -> Instruction {
+    Instruction::Mov(operand1, operand2)
+}
+
+pub fn cmp(operand1: Operand, operand2: Operand) -> Instruction {
+    Instruction::Cmp(operand1, operand2)
+}
+
+pub fn sete(operand: Operand) -> Instruction {
+    Instruction::Sete(operand)
+}
+
+pub fn setne(operand: Operand) -> Instruction {
+    Instruction::Setne(operand)
+}
+
+pub fn setl(operand: Operand) -> Instruction {
+    Instruction::Setl(operand)
+}
+
+pub fn setle(operand: Operand) -> Instruction {
+    Instruction::Setle(operand)
+}
+
 #[derive(Clone, Copy)]
 pub enum Operand {
     Register(Register),
     Immediate(i32),
     Memory(Register),
+}
+
+pub fn immediate(value: i32) -> Operand {
+    Operand::Immediate(value)
 }
 
 impl fmt::Debug for Operand {
@@ -107,4 +175,124 @@ impl fmt::Debug for Register {
             Register::AL => write!(f, "al"),
         }
     }
+}
+
+pub fn rax() -> Operand {
+    Operand::Register(Register::RAX)
+}
+
+pub fn m_rax() -> Operand {
+    Operand::Memory(Register::RAX)
+}
+
+pub fn rbp() -> Operand {
+    Operand::Register(Register::RBP)
+}
+
+pub fn m_rbp() -> Operand {
+    Operand::Memory(Register::RBP)
+}
+
+pub fn rdi() -> Operand {
+    Operand::Register(Register::RDI)
+}
+
+pub fn m_rdi() -> Operand {
+    Operand::Memory(Register::RDI)
+}
+
+pub fn rsi() -> Operand {
+    Operand::Register(Register::RSI)
+}
+
+pub fn m_rsi() -> Operand {
+    Operand::Memory(Register::RSI)
+}
+
+pub fn rdx() -> Operand {
+    Operand::Register(Register::RDX)
+}
+
+pub fn m_rdx() -> Operand {
+    Operand::Memory(Register::RDX)
+}
+
+pub fn rcx() -> Operand {
+    Operand::Register(Register::RCX)
+}
+
+pub fn m_rcx() -> Operand {
+    Operand::Memory(Register::RCX)
+}
+
+pub fn r8() -> Operand {
+    Operand::Register(Register::R8)
+}
+
+pub fn m_r8() -> Operand {
+    Operand::Memory(Register::R8)
+}
+
+pub fn r9() -> Operand {
+    Operand::Register(Register::R9)
+}
+
+pub fn m_r9() -> Operand {
+    Operand::Memory(Register::R9)
+}
+
+pub fn r10() -> Operand {
+    Operand::Register(Register::R10)
+}
+
+pub fn m_r10() -> Operand {
+    Operand::Memory(Register::R10)
+}
+
+pub fn r11() -> Operand {
+    Operand::Register(Register::R11)
+}
+
+pub fn m_r11() -> Operand {
+    Operand::Memory(Register::R11)
+}
+
+pub fn r12() -> Operand {
+    Operand::Register(Register::R12)
+}
+
+pub fn m_r12() -> Operand {
+    Operand::Memory(Register::R12)
+}
+
+pub fn r13() -> Operand {
+    Operand::Register(Register::R13)
+}
+
+pub fn m_r13() -> Operand {
+    Operand::Memory(Register::R13)
+}
+
+pub fn r14() -> Operand {
+    Operand::Register(Register::R14)
+}
+
+pub fn m_r14() -> Operand {
+    Operand::Memory(Register::R14)
+}
+
+pub fn r15() -> Operand {
+    Operand::Register(Register::R15)
+}
+
+pub fn m_r15() -> Operand {
+    Operand::Memory(Register::R15)
+}
+
+pub fn al() -> Operand {
+    Operand::Register(Register::AL)
+}
+
+pub fn m_al() -> Operand {
+    Operand::Memory(Register::AL)
 }
